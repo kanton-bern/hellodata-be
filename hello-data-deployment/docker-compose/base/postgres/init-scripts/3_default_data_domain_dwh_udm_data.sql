8c46ac63e5c2dc8aa7a1419620572fc66773f114
Simon Späti <simon.spaeti@bedag.ch>
2024-01-12 15:52:58 +0100

    shrink db file

--- hello-data-deployment/docker-compose/base/postgres/init-scripts/3_default_data_domain_dwh_udm_data.sql
+++ hello-data-deployment/docker-compose/base/postgres/init-scripts/3_default_data_domain_dwh_udm_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94adab0519434e43473ee5c2f1dfe6fd1e84c2628203a66efadfa3134569d92f
-size 765510331
+oid sha256:2ddd416d5f19d7cddc1a412bef328d564dfca9056011276f00f91e10850315a5
+size 48296173
