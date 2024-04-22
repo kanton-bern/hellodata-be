8c9077f9e65b05b1bd8200bd692170770585c36e
Slawomir Wieczorek <slawomir.wieczorek@bedag.ch>
2024-04-22 13:28:23 +0200

    HELLODATA-1334 - configurable dags

--- hello-data-deployment/hello-data-showcase/src/main/docker/showcase/dags/error_dag.py
+++ hello-data-deployment/hello-data-showcase/src/main/docker/showcase/dags/error_dag.py
@@ -1,17 +1,17 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow import AirflowException
+from datetime import datetime
+
 
 def raise_exception():
- raise AirflowFailException("Ooh nohhh!")
+    raise AirflowFailException("Ooh nohhh!")
 
-dag = DAG('error_dag', description='Intentional Error', schedule_interval=None, start_date=datetime(2017, 3, 20), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+dag = DAG('error_dag_' + "DD_KEY", description='Intentional Error', schedule_interval=None, start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
 exception_operator = PythonOperator(task_id='error_task', python_callable=raise_exception, dag=dag)
-
 
 dummy_operator >> exception_operator
--- hello-data-deployment/hello-data-showcase/src/main/docker/showcase/dags/hello_world.py
+++ hello-data-deployment/hello-data-showcase/src/main/docker/showcase/dags/hello_world.py
@@ -1,16 +1,17 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
 
 def print_hello():
- return 'Hello Wolrd'
+    return 'Hello Wolrd'
 
-dag = DAG('hello_world', description='Hello world example', schedule_interval=None, start_date=datetime(2017, 3, 20), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+dag = DAG('hello_world_' + "DD_KEY", description='Hello world example', schedule_interval=None, start_date=datetime(2017, 3, 20), catchup=False)
+
+dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
 
 dummy_operator >> hello_operator
