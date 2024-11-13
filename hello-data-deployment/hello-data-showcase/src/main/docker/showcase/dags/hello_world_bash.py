8e256dd21158de3824864dd7124933e4ccbe5067
Slawomir Wieczorek <slawomir.wieczorek@bedag.ch>
2024-11-13 10:47:41 +0100

    HELLODATA-1819 - add dag to test AIRFLOW_WEBSERVER_UPDATE_FAB_PERMS

---
+++ hello-data-deployment/hello-data-showcase/src/main/docker/showcase/dags/hello_world_bash.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+dag_name = 'dagName' + "DD_KEY"
+
+default_args = {
+    'description': 'Hello world example with bash usage',
+    'start_date': datetime(2024, 11, 12),
+    'schedule_interval': None,
+    'catchup': False
+}
+
+with DAG(dag_name, default_args=default_args) as dag:
+    t1 = DummyOperator(task_id="start")
+
+    t2 = BashOperator(task_id='hello_world',
+                      bash_command='echo "Hi!!"')
+
+    t3 = DummyOperator(task_id="end")
+
+    t1 >> t2 >> t3
