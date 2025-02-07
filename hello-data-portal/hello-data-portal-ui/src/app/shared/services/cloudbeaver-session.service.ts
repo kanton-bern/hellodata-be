359283f525cae0a857fccd9e378fbe028676951a
Slawomir Wieczorek <slawomir.wieczorek@bedag.ch>
2025-02-07 10:00:19 +0100

    HELLODATA-1887 - renew cloudbeaver session globally

---
+++ hello-data-portal/hello-data-portal-ui/src/app/shared/services/cloudbeaver-session.service.ts
@@ -0,0 +1,46 @@
+import {Store} from "@ngrx/store";
+import {AppState} from "../../store/app/app.state";
+import {interval} from "rxjs";
+import {renewCloudbeaverSession} from "../../store/auth/auth.action";
+
+export class CloudbeaverSessionService {
+
+  renewSessionInterval$ = interval(60000 * 5);
+  private readonly CLOUD_BEAVER_SESSION_COOKIE_NAME = 'cloudbeaver-session-renewal-timer';
+
+  constructor(private store: Store<AppState>) {
+  }
+
+  // call to maintain the session
+  public createInterval(): void {
+    this.createTimerCookie();
+    this.renewSessionInterval$
+      .subscribe(() => {
+        if (this.cookieExists()) {
+          this.store.dispatch(renewCloudbeaverSession());
+        } else {
+          console.error('No cookie found: ' + this.CLOUD_BEAVER_SESSION_COOKIE_NAME);
+        }
+      });
+  }
+
+  public destroyTimerCookie(): void {
+    document.cookie = `${this.CLOUD_BEAVER_SESSION_COOKIE_NAME}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/`;
+  }
+
+  private createTimerCookie(): void {
+    if (!this.cookieExists()) {
+      document.cookie = `${this.CLOUD_BEAVER_SESSION_COOKIE_NAME}=active;path=/`;
+    }
+  }
+
+  private cookieExists(): boolean {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+      if (cookie.trim().startsWith(`${this.CLOUD_BEAVER_SESSION_COOKIE_NAME}=`)) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
