c15da5fbde17ff9a56a3fb14496e3f4741645179
Slawomir Wieczorek <slawomir.wieczorek@bedag.ch>
2025-04-18 13:14:24 +0200

    HELLODATA-2252 - adding can favstar on Superset; can fave dashboards on Superset

---
+++ hello-data-subsystems/hello-data-superset/src/main/docker/db/changelog/changelogs/10_add_can_fave_dashboards_permission_to_bi_viewer.sql
@@ -0,0 +1,48 @@
+--
+-- Copyright © 2025, Kanton Bern
+-- All rights reserved.
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--     * Redistributions of source code must retain the above copyright
+--       notice, this list of conditions and the following disclaimer.
+--     * Redistributions in binary form must reproduce the above copyright
+--       notice, this list of conditions and the following disclaimer in the
+--       documentation and/or other materials provided with the distribution.
+--     * Neither the name of the <organization> nor the
+--       names of its contributors may be used to endorse or promote products
+--       derived from this software without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+-- ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+-- WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+-- DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+-- (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+-- LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+-- ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+-- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+
+--
+-- Adds a permission to the BI_VIEWER role to allow time range selection in the API view.
+--
+
+with role_ as (
+	select id from ab_role where "name" = 'BI_VIEWER'
+),
+permission_view_ as (
+	select pv.id from ab_permission_view pv
+		join ab_permission p on
+			pv.permission_id = p.id
+		join ab_view_menu vm on
+			pv.view_menu_id = vm.id
+	where
+		p."name" = 'can_fave_dashboards' and
+		vm."name" = 'Superset'
+)
+insert into ab_permission_view_role
+(id, permission_view_id, role_id) values
+(nextval('ab_permission_view_role_id_seq'), (select id from permission_view_ limit 1), (select id from role_ limit 1))
+on conflict do nothing;
