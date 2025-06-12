41524d7f0d47719543227a7e34fdfbfcbbbc7aa4
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-06-12 13:55:20 +0000

    HELLODATA-2446 - enhancements

--- hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/user/controller/UserController.java
+++ hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/user/controller/UserController.java
@@ -218,7 +218,7 @@ public class UserController {
     @PatchMapping("/{userId}/context-roles")
     @PreAuthorize("hasAnyAuthority('USER_MANAGEMENT')")
     public void updateContextRolesForUser(@PathVariable UUID userId, @NotNull @Valid @RequestBody UpdateContextRolesForUserDto updateContextRolesForUserDto) {
-        userService.updateContextRolesForUser(userId, updateContextRolesForUserDto);
+        userService.updateContextRolesForUser(userId, updateContextRolesForUserDto, true);
     }
 
     @GetMapping("search/{email}")
