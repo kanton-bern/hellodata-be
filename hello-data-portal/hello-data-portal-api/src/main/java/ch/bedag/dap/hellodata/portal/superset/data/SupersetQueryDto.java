fccf97365a796834bedfc56caed8028f0b2ba0b4
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-08-08 14:57:36 +0200

    HELLODATA-2411 - add missing uuid

--- hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/superset/data/SupersetQueryDto.java
+++ hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/superset/data/SupersetQueryDto.java
@@ -3,10 +3,19 @@ package ch.bedag.dap.hellodata.portal.superset.data;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Data
 public class SupersetQueryDto {
+
+    private UUID id;
+    private LocalDateTime createdDate;
+    private String createdBy;
+    private LocalDateTime modifiedDate;
+    private String modifiedBy;
+
     private String contextKey;
     private Integer subsystemId;
     private String databaseName;
