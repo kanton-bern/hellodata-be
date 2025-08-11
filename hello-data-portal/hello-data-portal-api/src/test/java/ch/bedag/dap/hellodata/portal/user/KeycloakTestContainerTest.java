924415ae04aa563002a087ff26a8262d91553d9e
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-08-11 08:25:12 +0200

    HELLODATA-2411 - mock bean

--- hello-data-portal/hello-data-portal-api/src/test/java/ch/bedag/dap/hellodata/portal/user/KeycloakTestContainerTest.java
+++ hello-data-portal/hello-data-portal-api/src/test/java/ch/bedag/dap/hellodata/portal/user/KeycloakTestContainerTest.java
@@ -32,6 +32,7 @@ import ch.bedag.dap.hellodata.portal.cache.service.CacheUpdateService;
 import ch.bedag.dap.hellodata.portal.initialize.service.RolesInitializer;
 import ch.bedag.dap.hellodata.portal.monitoring.service.StorageSizeService;
 import ch.bedag.dap.hellodata.portal.role.service.RoleService;
+import ch.bedag.dap.hellodata.portal.superset.service.QueryService;
 import ch.bedag.dap.hellodata.portal.user.service.BatchUsersCustomLogger;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.nats.client.Connection;
@@ -81,6 +82,8 @@ public abstract class KeycloakTestContainerTest {
     private CacheUpdateService cacheUpdateService;
     @MockBean
     private BatchUsersCustomLogger batchUsersCustomLogger;
+    @MockBean
+    private QueryService queryService;
 
     @DynamicPropertySource
     static void registerResourceServerIssuerProperty(DynamicPropertyRegistry registry) {
