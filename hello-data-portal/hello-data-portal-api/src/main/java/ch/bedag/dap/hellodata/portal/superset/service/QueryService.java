7a8dbbd3ab28e28fd97395d2cc40bd4a3bc4def5
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-08-11 12:51:50 +0200

    HELLODATA-2411 - conditional search

--- hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/superset/service/QueryService.java
+++ hello-data-portal/hello-data-portal-api/src/main/java/ch/bedag/dap/hellodata/portal/superset/service/QueryService.java
@@ -5,6 +5,7 @@ import ch.bedag.dap.hellodata.portalcommon.query.entity.QueryEntity;
 import ch.bedag.dap.hellodata.portalcommon.query.repository.QueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,7 +22,12 @@ public class QueryService {
 
     @Transactional(readOnly = true)
     public Page<SupersetQueryDto> fetchQueries(String contextKey, Pageable pageable, String search) {
-        Page<QueryEntity> allByContextKeyPageable = queryRepository.findAll(pageable, search, contextKey);
+        Page<QueryEntity> allByContextKeyPageable;
+        if (StringUtils.isNotBlank(search)) {
+            allByContextKeyPageable = queryRepository.findAll(pageable, search, contextKey);
+        } else {
+            allByContextKeyPageable = queryRepository.findAllByContextKey(pageable, contextKey);
+        }
         log.info("Fetched queries {} for contextKey: {}", allByContextKeyPageable, contextKey);
         return allByContextKeyPageable.map(userEntity -> modelMapper.map(userEntity, SupersetQueryDto.class));
     }
