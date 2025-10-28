fdc4ed076ca7a0c110b6bbd5ec721d87e9a8a000
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-10-28 13:27:36 +0100

    HELLODATA-2796 - severity: warn

--- hello-data-portal/hello-data-portal-ui/src/app/shared/components/published-announcement/published-announcements-popup/published-annoucements-popup-header/published-announcements-popup-header.component.ts
+++ hello-data-portal/hello-data-portal-ui/src/app/shared/components/published-announcement/published-announcements-popup/published-annoucements-popup-header/published-announcements-popup-header.component.ts
@@ -6,10 +6,12 @@ import {HideAllCurrentPublishedAnnouncementsService} from "../../hide-all-curren
   template: `
     <div class="grid">
       <div class="col-10">
-        <h3><b>{{'@Announcements' | transloco}}</b></h3>
+        <h3><b>{{ '@Announcements' | transloco }}</b></h3>
       </div>
       <div class="col">
-        <p-checkbox [binary]="true" inputId="dont-show-again" label="{{'@Do not show again' | transloco}}" (onChange)="onChange($event)"></p-checkbox>
+        <p-checkbox [binary]="true" inputId="dont-show-again"
+                    (onChange)="onChange($event)"></p-checkbox>
+        <label [for]="'dont-show-again'" class="ml-2"> {{ '@Do not show again' | transloco } </label>
       </div>
     </div>
   `,
