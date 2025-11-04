cd719a92a9fce5ae43ec7c73cb026ddcb04c0d14
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-11-04 14:35:52 +0100

    HELLODATA-2796 - fix label

--- hello-data-portal/hello-data-portal-ui/src/app/shared/components/published-announcement/published-announcements-popup/published-annoucements-popup-header/published-announcements-popup-header.component.ts
+++ hello-data-portal/hello-data-portal-ui/src/app/shared/components/published-announcement/published-announcements-popup/published-annoucements-popup-header/published-announcements-popup-header.component.ts
@@ -1,22 +1,22 @@
-import { Component, inject } from '@angular/core';
-import { CheckboxChangeEvent, Checkbox } from "primeng/checkbox";
+import {Component, inject} from '@angular/core';
+import {Checkbox, CheckboxChangeEvent} from "primeng/checkbox";
 import {HideAllCurrentPublishedAnnouncementsService} from "../../hide-all-current-published-announcements.service";
-import { TranslocoPipe } from '@jsverse/transloco';
+import {TranslocoPipe} from '@jsverse/transloco';
 
 @Component({
-    template: `
+  template: `
     <div class="grid">
       <div class="col-10">
         <h3><b>{{ '@Announcements' | transloco }}</b></h3>
       </div>
       <div class="col">
         <p-checkbox [binary]="true" inputId="dont-show-again"
-                    (onChange)="onChange($event)" />
-        <label for="dont-show-again" class="ml-2"> {{ '@Do not show again' | transloco } </label>
+                    (onChange)="onChange($event)"/>
+        <label for="dont-show-again" class="ml-2"> {{ '@Do not show again' | transloco }} </label>
       </div>
     </div>
   `,
-    imports: [Checkbox, TranslocoPipe]
+  imports: [Checkbox, TranslocoPipe]
 })
 export class PublishedAnnouncementsPopupHeaderComponent {
   private hideAllCurrentPublishedAnnouncementsService = inject(HideAllCurrentPublishedAnnouncementsService);
