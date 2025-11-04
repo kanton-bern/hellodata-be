f5038a3e9e7b669c0b6cdf7acd0e04f366479c34
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-11-04 09:29:06 +0100

    HELLODATA-2796 - fix dashboard access page

--- hello-data-portal/hello-data-portal-ui/src/app/pages/admin/dashboard-access/dashboard-access.component.ts
+++ hello-data-portal/hello-data-portal-ui/src/app/pages/admin/dashboard-access/dashboard-access.component.ts
@@ -25,7 +25,7 @@
 /// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ///
 
-import { Component, OnInit, inject, viewChild } from "@angular/core";
+import {Component, inject, OnInit, viewChild} from "@angular/core";
 import {BaseComponent} from "../../../shared/components/base/base.component";
 import {AppState} from "../../../store/app/app.state";
 import {Store} from "@ngrx/store";
@@ -36,7 +36,7 @@ import {
   selectDashboardAccessDataLoading,
   selectDashboardAccessTotalRecords
 } from "../../../store/dashboard-access/dashboard-access.selector";
-import { Table, TableLazyLoadEvent, TableModule } from "primeng/table";
+import {Table, TableLazyLoadEvent, TableModule} from "primeng/table";
 import {loadDashboardAccessPaginated} from "../../../store/dashboard-access/dashboard-access.action";
 import {selectSelectedDataDomain} from "../../../store/my-dashboards/my-dashboards.selector";
 import {scrollToTop} from "../../../shared/services/view-helpers";
@@ -44,18 +44,18 @@ import {naviElements} from "../../../app-navi-elements";
 import {createBreadcrumbs} from "../../../store/breadcrumb/breadcrumb.action";
 import {DataDomain} from "../../../store/my-dashboards/my-dashboards.model";
 import {map} from "rxjs/operators";
-import { AsyncPipe, DatePipe } from "@angular/common";
-import { PrimeTemplate } from "primeng/api";
-import { IconField } from "primeng/iconfield";
-import { InputIcon } from "primeng/inputicon";
-import { FormsModule } from "@angular/forms";
-import { InputText } from "primeng/inputtext";
-import { TranslocoPipe } from "@jsverse/transloco";
+import {AsyncPipe, DatePipe} from "@angular/common";
+import {PrimeTemplate} from "primeng/api";
+import {IconField} from "primeng/iconfield";
+import {InputIcon} from "primeng/inputicon";
+import {FormsModule} from "@angular/forms";
+import {InputText} from "primeng/inputtext";
+import {TranslocoPipe} from "@jsverse/transloco";
 
 @Component({
-    templateUrl: 'dashboard-access.component.html',
-    styleUrls: ['./dashboard-access.component.scss'],
-    imports: [TableModule, PrimeTemplate, IconField, InputIcon, FormsModule, InputText, AsyncPipe, DatePipe, TranslocoPipe]
+  templateUrl: 'dashboard-access.component.html',
+  styleUrls: ['./dashboard-access.component.scss'],
+  imports: [TableModule, PrimeTemplate, IconField, InputIcon, FormsModule, InputText, AsyncPipe, DatePipe, TranslocoPipe]
 })
 export class DashboardAccessComponent extends BaseComponent implements OnInit {
   private store = inject<Store<AppState>>(Store);
@@ -64,7 +64,7 @@ export class DashboardAccessComponent extends BaseComponent implements OnInit {
   selectedDataDomain$: Observable<DataDomain | null>;
   filterValue = '';
   first = 0;
-  readonly table = viewChild.required<Table>('dt');
+  readonly table = viewChild<Table>('dt');
   totalRecords = 0;
   dataLoading$ = this.store.select(selectDashboardAccessDataLoading);
   usedContextKey: string | null = null;
