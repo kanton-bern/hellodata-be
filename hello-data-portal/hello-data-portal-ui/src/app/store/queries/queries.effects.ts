7d13ae147b9ac267a9562cf8eb787bb3e22f7bd0
Slawomir Wieczorek <slawomirwieczorek@gmail.com>
2025-11-14 10:29:13 +0100

    HELLODATA-2763 - redundant injection

--- hello-data-portal/hello-data-portal-ui/src/app/store/queries/queries.effects.ts
+++ hello-data-portal/hello-data-portal-ui/src/app/store/queries/queries.effects.ts
@@ -4,14 +4,11 @@ import {asyncScheduler, catchError, scheduled, switchMap} from "rxjs";
 import {showError} from "../app/app.action";
 import {loadQueriesPaginated, loadQueriesSuccess} from "./queries.action";
 import {QueriesService} from "./queries.service";
-import {Store} from "@ngrx/store";
-import {AppState} from "../app/app.state";
 
 @Injectable()
 export class QueriesEffects {
   private _actions$ = inject(Actions);
   private _queriesService = inject(QueriesService);
-  private _store = inject<Store<AppState>>(Store);
 
 
   loadQueriesPaginated$ = createEffect(() => {
